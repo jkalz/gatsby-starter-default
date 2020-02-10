7893044bd5cb32b98c3a334fb26d7625313a62f8
jkalz <60848662+jkalz@users.noreply.github.com>
2020-02-10 23:09:27 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people from Gatsbyjs</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <p>Welcome to your new Gatsby site. I am so happy!</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
